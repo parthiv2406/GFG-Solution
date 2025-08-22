435744020ac07d052fa7115c7d357d09407efc21
Parthiv Das Mandal <130141458+parthiv2406@users.noreply.github.com>
2025-08-22 11:16:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -2,7 +2,7 @@ class Solution {
     static int factorial(int n) {
         // code here
         int res = 1;
-        for(int i=1; i<=n; i++){
+        for(int i=2; i<=n; i++){
             res = res * i;
         }
         return res;
