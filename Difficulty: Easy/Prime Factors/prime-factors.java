fbc51703700daebc0046a2d32e1f8fd75d9668de
Parthiv Das Mandal <130141458+parthiv2406@users.noreply.github.com>
2025-08-22 12:30:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Factors/prime-factors.java
+++ Difficulty: Easy/Prime Factors/prime-factors.java
@@ -1,22 +1,16 @@
 class Solution {
-    static boolean isPrime(int num){
-        if(num<=3) return true;
-        if(num%2==0 || num %3==0) return false;
-        for(int i=5 ; i*i<=num; i+=6){
-            if(num%i==0 || num%(i+2)==0)return false;
-        }
-        return true;
-    }
     public static ArrayList<Integer> primeFac(int n) {
         // code here
         ArrayList<Integer> arr = new ArrayList<>();
-        for(int i=2; i<=n ; i++){
-            if(isPrime(i)==true){
-                if(n%i==0){
-                    arr.add(i);
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0){
+                arr.add(i);
+                while(n%i==0){
+                    n = n/i;
                 }
             }
         }
+        if(n!=1) arr.add(n);
         return arr;
     }
 }
